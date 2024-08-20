fa87a511cbf3164638be0a5faffe84d7852b59eb
ckdt <ronny@allthis.digital>
2024-08-20 16:10:20 +0200

    nieuw

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import lottie from "astro-integration-lottie";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    lottie(),
-  ]
+  integrations: [lottie(), icon()]
 });
